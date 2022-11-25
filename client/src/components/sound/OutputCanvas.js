48aa4c6abcd4890577c8675e8c9ef3a50efbeabd
AdneCode <super-gijs@hotmail.com>
2022-11-25 15:13:49 +0100

    feature: added glow to canvas notes

--- client/src/components/sound/OutputCanvas.js
+++ client/src/components/sound/OutputCanvas.js
@@ -12,20 +12,25 @@ const OutputCanvas = (p) => {
 
         sounds.map((i) => {
             let color = '#2563eb';
+
+            ctx.fill();
             ctx.beginPath();
             ctx.globalAlpha = 1;
-            if (i.xPosition < 110) {
-                ctx.globalAlpha = (i.xPosition - 10) / 100;
+            ctx.shadowBlur = 5;
+            ctx.shadowColor = color;
+            if (i.xPosition < 60) {
+                ctx.globalAlpha = (i.xPosition - 10) / 50;
             }
             if (i.xPosition < 10) {
                 ctx.globalAlpha = 0;
             }
             ctx.fillStyle = color;
             const soundType = getTypeFromOutput(i.output);
-
             if (soundType === 'drum') {
                 if (i.xPosition > 244 && i.output === 'sound/steban/kick.mp3') {
                     ctx.arc(i.xPosition, i.height, 5, 0, 2 * Math.PI);
+                    ctx.fillStyle = color;
+                    ctx.rect(i.xPosition, 1, 1, ctx.canvas.clientHeight);
                 }
                 if (i.xPosition > 244) {
                     ctx.arc(i.xPosition, i.height, 3, 0, 2 * Math.PI);
@@ -35,6 +40,8 @@ const OutputCanvas = (p) => {
                     i.xPosition <= 244
                 ) {
                     ctx.arc(i.xPosition, i.height, 3, 0, 2 * Math.PI);
+                    ctx.fillStyle = color;
+                    ctx.rect(i.xPosition, 1, 1, ctx.canvas.clientHeight);
                 }
                 if (i.xPosition <= 244) {
                     ctx.arc(i.xPosition, i.height, 2, 0, 2 * Math.PI);
@@ -43,9 +50,14 @@ const OutputCanvas = (p) => {
                 return;
             }
             if (soundType === 'piano') {
-                if (i.xPosition > 244) {
-                    const difference = i.xPosition - 244;
-                    ctx.rect(i.xPosition + difference, i.height, difference, 2);
+                if (i.xPosition > 242) {
+                    const difference = i.xPosition - 242;
+                    ctx.rect(
+                        i.xPosition + difference,
+                        i.height - 1,
+                        difference,
+                        4,
+                    );
                     ctx.fill();
                     return;
                 }
@@ -58,11 +70,13 @@ const OutputCanvas = (p) => {
 
     useEffect(() => {
         const canvas = canvasRef.current;
-        const context = canvas.getContext('2d');
-        context.fillStyle = '#4b5563';
-        context.fillRect(0, 0, context.canvas.width, context.canvas.height);
+        const ctx = canvas.getContext('2d');
+        ctx.fillStyle = '#4b5563';
+        ctx.shadowBlur = 7;
+        ctx.shadowColor = '#4b5563';
+        ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
         if (!sounds || sounds.length === 0) {
-            context.clearRect(0, 0, canvas.width, canvas.height);
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
         }
     }, [sounds]);
 
