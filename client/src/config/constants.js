f188f020ff8efcdeb3af0c8c8ce80b7882ae0f68
Patrick van Everdingen <patrick@passionatepeople.io>
2022-11-26 21:55:09 +0100

    chore: change backend services ip

--- client/src/config/constants.js
+++ client/src/config/constants.js
@@ -1,4 +1,4 @@
-const url = 'http://192.168.0.118';
+const url = 'http://134.122.103.104/';
 export const apiUrl = process.env.API_URL || `${url}:4000`;
 export const socketUrl = `${url}:4001`;
 export const DEFAULT_MESSAGE_TIMEOUT = 3000;
