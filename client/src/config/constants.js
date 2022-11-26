2fb1cf714b27a48b33f941797518fc16c6444188
Patrick van Everdingen <patrick@passionatepeople.io>
2022-11-26 22:04:26 +0100

    chore: fix typo

--- client/src/config/constants.js
+++ client/src/config/constants.js
@@ -1,4 +1,4 @@
 const url = 'http://134.122.103.104';
-export const apiUrl = process.env.API_URL || `${url}:4000`;
+export const apiUrl = url;
 export const socketUrl = `${url}:4001`;
 export const DEFAULT_MESSAGE_TIMEOUT = 3000;
