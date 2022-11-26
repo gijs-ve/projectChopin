a980333508a47a099f286285188cdc5136ad4402
AdneCode <super-gijs@hotmail.com>
2022-11-26 10:53:18 +0100

    fix: adjusted recordingsPage

--- client/src/components/classNames.js
+++ client/src/components/classNames.js
@@ -2,3 +2,5 @@ export const cnButton =
     'inline-flex items-center px-6 py-3 ml-4 border border-transparent text-base font-medium rounded-full shadow-sm text-white bg-blue-600 hover:bg-blue-700';
 export const cnButtonUnbound =
     'inline-flex items-center px-6 py-3 ml-4 border border-transparent text-base font-medium rounded-full shadow-sm text-white bg-red-600 hover:bg-red-700';
+export const whiteLabel =
+    'text-white group flex items-center px-2 py-2 text-base font-medium rounded-md';
--- client/src/pages/RecordingsPage.js
+++ client/src/pages/RecordingsPage.js
@@ -1,7 +1,13 @@
 import { RecordListener } from '../components';
-
+import { whiteLabel } from '../components/classNames';
 function RecordingsPage() {
-    return <RecordListener />;
+    return (
+        <>
+            <h1 className={whiteLabel}>My recordings</h1>
+            <h1 className={whiteLabel}>Shared recordings</h1>
+            <h1 className={whiteLabel}>Public recordings</h1>
+        </>
+    );
 }
 
 export { RecordingsPage };
