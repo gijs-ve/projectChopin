d5f88c311c2e6695fa5686ad5f7a3856864a30f6
AdneCode <super-gijs@hotmail.com>
2022-11-28 14:34:00 +0100

    feature: added edit router

--- server/routers/settings.js
+++ server/routers/settings.js
@@ -7,14 +7,41 @@ router.patch('/edit', authMiddleware, async (req, res) => {
     try {
         const { user } = req;
         const { settings } = req.body;
-        const foundRecord = await Settings.findOne({
+
+        const foundSettings = await Settings.findOne({
             where: { userId: user.id },
         });
-        if (req.user.id !== foundRecord.userId)
+        if (req.user.id !== foundSettings.userId)
             return res.status(403).send({
                 message: 'You have no access to edit these settings.',
             });
-        foundRecord.update({ name });
+        const setting = settings.map((i) => {
+            return [i.name.split(' ').join('').toLowerCase(), i.setting];
+        });
+        const userSettings = {};
+        setting.forEach((i) => {
+            const key = i[0];
+            const value = i[1];
+            userSettings[key] = value;
+        });
+        const {
+            imageurl,
+            hexcolor,
+            enableinstrument,
+            showpresets,
+            showdisplayer,
+            showrecorder,
+        } = userSettings;
+        console.log(hexcolor);
+        await foundSettings.update({
+            imageURL: imageurl,
+            color: hexcolor,
+            showInstrumentButtons: enableinstrument,
+            showPresetButtons: showpresets,
+            displayerOn: showdisplayer,
+            recorderOn: showrecorder,
+        });
+
         return res
             .status(200)
             .send({ message: 'Record was succesfully renamed!' });
