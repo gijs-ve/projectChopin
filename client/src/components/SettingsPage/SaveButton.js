3f4b58f13b657c1d7c0d0e45a354fad6703e44fa
AdneCode <super-gijs@hotmail.com>
2022-11-28 13:50:39 +0100

    fix: adjusted save button for handler event

--- client/src/components/SettingsPage/SaveButton.js
+++ client/src/components/SettingsPage/SaveButton.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { cnButton } from '../classNames';
 import { useDispatch } from 'react-redux';
 
-function SaveButton(settings, handler) {
+function SaveButton(p) {
+    const { settings, handler } = p;
     const dispatch = useDispatch();
     return (
         <div className={cnButton} onClick={() => dispatch(handler(settings))}>
