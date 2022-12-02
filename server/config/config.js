3540662beb157286ea5d9d832100b5d11fc4ab09
AdneCode <super-gijs@hotmail.com>
2022-12-02 12:12:49 +0100

    Updated config

--- server/config/config.js
+++ server/config/config.js
@@ -5,18 +5,8 @@ module.exports = {
         url: process.env.DB_CONNECTION_URI,
         dialect: 'postgres',
     },
-    test: {
-        username: 'root',
-        password: null,
-        database: 'database_test',
-        host: '127.0.0.1',
-        dialect: 'mysql',
-    },
     production: {
-        username: 'root',
-        password: null,
-        database: 'database_production',
-        host: '127.0.0.1',
-        dialect: 'mysql',
+        url: process.env.DB_CONNECTION_URI,
+        dialect: 'postgres',
     },
 };
