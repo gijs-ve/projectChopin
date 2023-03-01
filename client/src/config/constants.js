c55ea76397f19f7e2240177a9493feb14f2cec44
AdneCode <super-gijs@hotmail.com>
2023-03-01 18:43:32 +0100

    fix: ip

--- client/src/config/constants.js
+++ client/src/config/constants.js
@@ -1,4 +1,4 @@
-//Zconst url = 'http://192.168.0.118:4000';
+const url = 'http://192.168.0.118:4000';
 //const url = "http://192.168.1.115:4000"
 export const apiUrl = `${url}`;
 export const socketUrl = `${url}`;
