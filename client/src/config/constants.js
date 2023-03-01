3bf18fa940c339d11dfd557d23071aa2af92b2e2
AdneCode <super-gijs@hotmail.com>
2023-03-01 18:43:21 +0100

    fix: local url

--- client/src/config/constants.js
+++ client/src/config/constants.js
@@ -1,4 +1,5 @@
-const url = process.env.REACT_APP_SERVER_URL || 'http://192.168.0.118:4000';
+//Zconst url = 'http://192.168.0.118:4000';
+//const url = "http://192.168.1.115:4000"
 export const apiUrl = `${url}`;
 export const socketUrl = `${url}`;
 export const DEFAULT_MESSAGE_TIMEOUT = 3000;
