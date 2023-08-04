7e54311a3a1cac6f6704f6c82b98964b89e2699c
Gijs <super-gijs@hotmail.com>
2023-08-04 15:12:04 +0200

    fix: port

--- client/src/config/constants.js
+++ client/src/config/constants.js
@@ -1,4 +1,4 @@
-const url = 'beathoven-server-production.up.railway.app:4000';
+const url = 'beathoven-server-production.up.railway.app:5463';
 //const url = "http://192.168.1.115:4000"
 export const apiUrl = `${url}`;
 export const socketUrl = `${url}`;
