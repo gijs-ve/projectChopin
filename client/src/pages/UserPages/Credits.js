cc5aaa7b09235b9f05631f2c9e23a2829f861244
Gijs <super-gijs@hotmail.com>
2023-08-04 15:35:24 +0200

    feat: version

--- client/src/pages/UserPages/Credits.js
+++ client/src/pages/UserPages/Credits.js
@@ -4,7 +4,7 @@ import { cnCreditsText, whiteLabel } from '../../components/classNames';
 function Credits() {
     return (
         <div className="flex flex-col flex-wrap border-4 rounded-xl border-gray-300 content-center text-center border-2 bg-gray-400 mx-[20%] px-[12%] py-[3%]">
-            <h1 className={cnCreditsText}>BeatHoven</h1>
+            <h1 className={cnCreditsText}>BeatHoven v0.2</h1>
             <h1 className={whiteLabel + ' self-end'}>
                 by Gijsbert van Everdingen
             </h1>
